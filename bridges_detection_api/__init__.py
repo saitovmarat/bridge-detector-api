5c06960410ce466995277e11ad8cc1e8baec14d7
saitovmarat <maratsajtov@gmail.com>
2025-06-28 23:29:36 +0300

    repo init

---
+++ bridges_detection_api/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for bridges-detection-api."""
+
+__author__ = """Saitov Marat Ayratovich"""
+__email__ = 'maratsajtov@gmail.com'
+__version__ = '0.1.0'
---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use bridges-detection-api in a project::
+
+    import bridges_detection_api
