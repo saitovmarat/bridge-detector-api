056428b1e60f258f2d2c5bef51deca618e1151d9
saitovmarat <maratsajtov@gmail.com>
2025-07-21 18:15:22 +0300

    small improvements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="bridges_detection_api",
     version="0.1.0",
-    packages=find_packages(),
+    packages=[
+        'bridges_detection_api',
+    ],
+    package_dir={'bridges_detection_api':
+                 'bridges_detection_api'},
     include_package_data=True,
+    license="MIT license",
+    test_suite='tests'
 )
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from bridges_detection_api.app import create_app
-from bridges_detection_api.settings import DevConfig
 
-app = create_app(config_object=DevConfig)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
