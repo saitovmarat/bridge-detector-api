a96d80e67e00189e041433d1c98e7bfa7e0c4303
saitovmarat <maratsajtov@gmail.com>
2025-08-01 17:23:17 +0300

    integrated udp server

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from bridges_detection_api.app import create_app
+# from bridges_detection_api.app import create_app
+from bridges_detection_api.app import create_udp_server
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+#   app.run(debug=True, host='0.0.0.0', port=5000)
+    create_udp_server(host="0.0.0.0", port=9999)
