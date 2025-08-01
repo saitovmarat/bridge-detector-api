e2b14f8e68512b0ae1f9a48bb0bf0923283adaba
saitovmarat <maratsajtov@gmail.com>
2025-08-01 17:22:58 +0300

    chaged the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-from bridges_detection_api.app import create_app
+from bridges_detection_api.app import create_rest_server
 from bridges_detection_api.settings import TestConfig
 
 
 @pytest.fixture(scope='function')
 def app():
-    return create_app(TestConfig)
+    return create_rest_server(TestConfig)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,20 @@
-from bridges_detection_api.app import create_app
+from bridges_detection_api.app import create_rest_server
 from bridges_detection_api.settings import DevConfig
-from bridges_detection_api.rest.detect_api import blueprint
+from bridges_detection_api.api.rest_api import blueprint
 
 
 def test_create_app_returns_app_instance():
-    app = create_app()
+    app = create_rest_server()
     assert app is not None
     assert app.name == 'bridges_detection_api.app'  
 
 
 def test_create_app_uses_correct_config():
-    app = create_app(config_object=DevConfig)
+    app = create_rest_server(config_object=DevConfig)
     assert app.config['DEBUG'] is True  
 
 
 def test_create_app_registers_blueprint():
-    app = create_app()
+    app = create_rest_server()
     assert blueprint.name in app.blueprints 
 
